b74ddadca322f9b09d1c24fc234c78c58b023945
Ralf Guth <rsguth@gmail.com>
2021-06-07 17:34:35 -0300

    limpa o projeto cria o componente head cria o css

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Task Tracker</h1>
+            <button className="btn">Adicionar</button>
+        </header>
+    )
+}
+
+export default Header
