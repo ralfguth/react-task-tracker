af1ee2c9fdbcf271adacac285dbec31a8682d71f
Ralf Guth <rsguth@gmail.com>
2021-06-08 14:16:06 -0300

    criando lista modelo como estado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks , setTasks] = useState(
+    [
+        {
+          id: 1,
+          text: "Levar o Lixo",
+          day: "5 de Fev às 14:15h",
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: "Estudar React",
+          day: "6 de Fev às 08:00h",
+          reminder: true,
+        },
+        {
+          id: 3,
+          text: "Estudar SQL",
+          day: "7 de Fev às 08:00h",
+          reminder: false,
+        },
+      ]
+);
   return (
     <div className="container-fluid">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,34 +1,11 @@
-import { useState } from 'react'
+import Task from './Task'
 
-
-
-const Tasks = () => {
-    const [tasks , setTasks] = useState(
-        [
-            {
-              id: 1,
-              text: "Levar o Lixo",
-              day: "5 de Fev às 14:15h",
-              reminder: true,
-            },
-            {
-              id: 2,
-              text: "Estudar React",
-              day: "6 de Fev às 08:00h",
-              reminder: true,
-            },
-            {
-              id: 3,
-              text: "Estudar SQL",
-              day: "7 de Fev às 08:00h",
-              reminder: false,
-            },
-          ]
-    );
+const Tasks = ({ tasks }) => {
+    
   return (
     <>
       {tasks.map((task) => (
-        <h3 id={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
