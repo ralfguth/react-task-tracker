dbcd66e8c59fa07c9d19ab62d9d1a550181cf294
Ralf Guth <rsguth@gmail.com>
2021-06-08 15:41:11 -0300

    implementa alguns metodos de manipulacao do estado

--- src/App.js
+++ src/App.js
@@ -3,32 +3,45 @@ import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
-  const [tasks , setTasks] = useState(
+  const [tasks, setTasks] = useState(
     [
-        {
-          id: 1,
-          text: "Levar o Lixo",
-          day: "5 de Fev às 14:15h",
-          reminder: true,
-        },
-        {
-          id: 2,
-          text: "Estudar React",
-          day: "6 de Fev às 08:00h",
-          reminder: true,
-        },
-        {
-          id: 3,
-          text: "Estudar SQL",
-          day: "7 de Fev às 08:00h",
-          reminder: false,
-        },
-      ]
-);
+      {
+        id: 1,
+        text: "Levar o Lixo",
+        day: "5 de Fev às 14:15h",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Estudar React",
+        day: "6 de Fev às 08:00h",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Estudar SQL",
+        day: "7 de Fev às 08:00h",
+        reminder: false,
+      },
+    ]
+  );
+
+  // Deletar Tarefa
+  const deletarTarefa = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Alternar Lembrete
+  const lembrete = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
-    <div className="container-fluid">
+    <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deletarTarefa} onToggle={lembrete} />
+      ) : 'Sem tarefas cadastradas'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Button from "./Button"
 const Header = () => {
     return (
         <header className="header">
-            <h1 textColor>Task Tracker</h1>
+            <h1>Task Tracker</h1>
             <Button color="#05AFF2" text="Adicionar"/>
         </header>
     )
